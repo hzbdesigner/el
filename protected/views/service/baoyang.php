2e08d93f0d9057dba00270a6a8a230c5c1c98525
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-03-13 01:14:39 +0800

    添加百度地图和一些配图
    只剩下：保养维护的文字，反馈页面，全国网点页面，另外加上新闻，活动两处。

--- protected/views/service/baoyang.php
+++ protected/views/service/baoyang.php
@@ -1,11 +1,7 @@
+<div class="" ><img style="width:515px;height:auto;" src="<?php echo Yii::app()->baseUrl; ?>/images/subbanner-baoyang.jpg" /></div>
 
-<div class="des">
-	<h2>保养维护</h2>
-	<p>配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述</p>
-	<p>配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述</p>
-	<p>配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述配送服务描述</p>
+<div style="width:480px;margin-left:15px;margin-top:10px;">
+	<h2 style="font-size:16px;margin-bottom:5px;color:#fff;">保养维护</h2>
+
+	<p style="font-size:12px;color:#fff;line-height:16px;margin-bottom:15px;">保养维护保养维护保养维护保养维护保养维护保养维护保养维护保养维护保养维护保养维护保养维护保养维护保养维护</p>
 </div>
-<div class="img"><img src="<?php echo Yii::app()->baseUrl; ?>/images/service-peisong.jpg" /></div>
-
-
-
