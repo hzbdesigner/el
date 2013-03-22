3fb59a4bb511fc09ea37fff2bd13aba2ebecd863
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-03-22 15:30:42 +0800

    3.22fix3.0

--- protected/views/service/liuyan.php
+++ protected/views/service/liuyan.php
@@ -10,17 +10,17 @@
 <div style="color:#666 !important; font-size:12px;position:relative;">
 	<form  style="padding-left:15px;padding-top:20px;">
 		<p>
-			<input class="span2" style="margin-bottom:15px;" type="text" value="您的姓名">
-			<input class="span2" style="margin-bottom:15px;margin-left:15px;" type="text" value="您的联系电话">
+			<input class="span2" style="margin-bottom:15px;padding:5px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="您的姓名">
+			<input class="span2" style="margin-bottom:15px;margin-left:15px;padding:5px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="您的联系电话">
 		</p>
 		<p>
-			<input class="span4" style="margin-bottom:15px;"  type="text" value="电子邮箱">
+			<input class="span4" style="margin-bottom:15px;padding:5px; background-color:transparent;color:#886239;border-color:#886239;"  type="text" value="电子邮箱">
 		</p>
 		<p>
-			<input class="span4" style="margin-bottom:15px;" type="text" value="内容主题">
+			<input class="span4" style="margin-bottom:15px;padding:5px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="内容主题">
 		</p>
 		<p>
-			<textarea class="span5" style="margin-bottom:15px;min-height:60px;"  type="text" value="请输入您的留言反馈">请输入您的留言反馈</textarea>
+			<textarea class="span5" style="margin-bottom:15px;min-height:60px;padding:5px; background-color:transparent;color:#886239;border-color:#886239;"  type="text" value="请输入您的留言反馈">请输入您的留言反馈</textarea>
 		</p>
 		<button class="btn">提&nbsp;&nbsp;交</button>
 	</form>
