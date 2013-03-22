d7072660aeb98738126fe31481f7990973a19b4f
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-03-22 15:54:41 +0800

    3.22fix4.2

--- protected/views/service/liuyan.php
+++ protected/views/service/liuyan.php
@@ -8,19 +8,19 @@
 <!-- <div class="img"><img src="<?php echo Yii::app()->baseUrl; ?>/images/service-peisong.jpg" /></div> -->
 <div class="" ><img style="width:515px;height:auto;" src="<?php echo Yii::app()->baseUrl; ?>/images/subbanner-jianjie.jpg" /></div>
 <div style="color:#666 !important; font-size:12px;position:relative;">
-	<form  style="padding-left:15px;padding-top:20px;">
+	<form  style="padding-left:15px;padding-top:12px;">
 		<p>
-			<input class="span2" style="margin-bottom:10px;font-size:12px;padding:3px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="您的姓名">
-			<input class="span2" style="margin-bottom:10px;font-size:12px;margin-left:15px;padding:3px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="您的联系电话">
+			<input class="span2" style="margin-bottom:8px;font-size:12px;padding:3px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="您的姓名">
+			<input class="span2" style="margin-bottom:8px;font-size:12px;margin-left:15px;padding:3px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="您的联系电话">
 		</p>
 		<p>
-			<input class="span4" style="margin-bottom:10px;font-size:12px;padding:3px; background-color:transparent;color:#886239;border-color:#886239;"  type="text" value="电子邮箱">
+			<input class="span4" style="margin-bottom:8px;font-size:12px;padding:3px; background-color:transparent;color:#886239;border-color:#886239;"  type="text" value="电子邮箱">
 		</p>
 		<p>
-			<input class="span4" style="margin-bottom:10px;font-size:12px;padding:3px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="内容主题">
+			<input class="span4" style="margin-bottom:8px;font-size:12px;padding:3px; background-color:transparent;color:#886239;border-color:#886239;" type="text" value="内容主题">
 		</p>
 		<p>
-			<textarea class="span5" style="margin-bottom:10px;font-size:12px;min-height:60px;padding:3px; background-color:transparent;color:#886239;border-color:#886239; "  type="text" value="请输入您的留言反馈">请输入您的留言反馈</textarea>
+			<textarea class="span5" style="margin-bottom:8px;font-size:12px;min-height:60px;padding:3px; background-color:transparent;color:#886239;border-color:#886239; "  type="text" value="请输入您的留言反馈">请输入您的留言反馈</textarea>
 		</p>
 		<button class="btn" style="background-color:transparent !important;background-image:none;border-color:#886239; color:#444;">提&nbsp;&nbsp;交</button>
 	</form>
