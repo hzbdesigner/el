18a1a5fd93000e556f6b1dfe5f5eab490b3f1a21
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-03-26 13:07:19 +0800

    user_login功能

--- protected/modules/admin/views/default/index.php
+++ protected/modules/admin/views/default/index.php
@@ -1,19 +1,20 @@
 
+
 	<div class="container login">
-		<form>
+		<form class="bs-docs-example form-horizontal" action='<?php echo $this->createUrl('/admin/default/login') ?>' method='post'>
 			<fieldset>
 				<legend>管理员登录</legend>
 				<div class="control-group">
 					<span>用户名</span>
-					<input type="text"   placeholder="输入用户名" />
+					<input type="text"   placeholder="输入用户名" name='LoginForm[email]'/>
 				</div>
 				<div class="control-group">
 					<span>密&nbsp;&nbsp;&nbsp;码</span>
-					<input type="text"   placeholder="输入密码" />
+					<input type="password"   placeholder="输入密码" name='LoginForm[password]'/>
 				</div>
 				<div class="control-group">
-					<a class="btn" href="<?php echo $this->createUrl('/admin/product/index'); ?>">登&nbsp;&nbsp;录</a>
-					<label><input type="checkbox">记住我</label>
+					<button class="btn" >登&nbsp;&nbsp;录</button>
+					<label><input type="checkbox" name='LoginForm[rememberMe]'>记住我</label>
 				</div>
 
 			</fieldset>
