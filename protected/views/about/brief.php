71663130666e2ee05bf27c836eb0a49880c2f409
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-04-07 11:19:20 +0800

    4.7fix

--- protected/views/about/brief.php
+++ protected/views/about/brief.php
@@ -4,6 +4,8 @@
 
 <div style="width:480px;margin-left:15px;margin-top:10px;">
 	<h2 style="font-size:16px;margin-bottom:5px;color:#fff;">公司简介</h2>
+	
+
 	<p style="font-size:12px;color:#fff;line-height:16px;">北京东方传奇家具有限公司是以经营进口家具，高端定制家具，饰品，床品，名画等为一体的综合性家具公司。公司代理多个美国高端家具品牌，如：Karges，Henkel Harris，Maitland－Smith，Henredon，Our house, Jonathan Charles ，这些品牌从设计构思到生产制造上无不力求卓越，而其中的Karges，Henkel Harris更是纯手工制作的各界名流的御用家私，享誉内外，无与伦比。</p>
 	
 </div>
